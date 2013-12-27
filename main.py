67e04fe7223539f8c354042cb20a0ca0a4a786de
Ciro Costa <ciro.costa@usp.br>
2013-12-27 00:52:01 -0200

    Arquivos e templates colocados em lugar, iniciada a criacao dos mesmos

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
 import webapp2
-import index.views
 import settings
+import index.views
+import votescreen.views
+
 
 application = webapp2.WSGIApplication([
     ('/', index.views.IndexPage),
+    ('/votescreen',votescreen.views.VoteScreen),
+    ('/votescreen/einfo',votescreen.views.EInfo),
+    ('/votescreen/vinfo',votescreen.views.VInfo),
 ], debug=settings.DEBUG)
---
+++ votescreen/views.py
@@ -0,0 +1,28 @@
+import webapp2
+import settings
+
+class VoteScreen(webapp2.RequestHandler):
+    def get(self):
+        template_values = {
+            
+        }
+        template = settings.JINJA_ENVIRONMENT.get_template('votescreen/votescreen.html')
+        self.response.write(template.render(template_values))
+
+class EInfo(webapp2.RequestHandler):
+    def get(self):
+        template_values = {
+            
+        }
+        template = settings.JINJA_ENVIRONMENT.get_template('votescreen/einfo.html')
+        self.response.write(template.render(template_values))
+
+
+class VInfo(webapp2.RequestHandler):
+    def get(self):
+        template_values = {
+            
+        }
+        template = settings.JINJA_ENVIRONMENT.get_template('votescreen/vinfo.html')
+        self.response.write(template.render(template_values))
+
